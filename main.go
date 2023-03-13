fa9be3965a9c305f5a5212527c7daf46bc188626
Ferdz <ferdz.steenkamp@gmail.com>
2023-03-13 18:51:18 +0200

    none

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"os"
+	"rock_ed/todo"
+)
+
+var (
+	//go:embed store.json
+	store embed.FS
+)
 
 func main() {
-	fmt.Println("Hello world")
+	db := openDB()
+	todoStore := todo.NewFsStore(db)
+
+	fmt.Println(todoStore.List(todo.Checked))
+}
+
+func openDB() *os.File {
+	file, err := store.Open("store.json")
+	exitIfErr(err)
+
+	info, err := file.Stat()
+	exitIfErr(err)
+
+	db, err := os.OpenFile(info.Name(), os.O_RDWR, 0666)
+	exitIfErr(err)
+
+	return db
+}
+
+func exitIfErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
